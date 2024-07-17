bd936365c15109f84ef5fb539e5bfe3c39ed21c2
Victor Felipe Jr <23023987@saultcollege.ca>
2024-07-17 06:12:29 -0400

    testing features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,34 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import {RouterModule, RouterOutlet} from '@angular/router';
 import { trigger, transition, style, query, group, animate } from '@angular/animations';
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, RouterOutlet, FooterComponent],
   template: `
-    <nav>
-        <a class="meHome" routerLink="/">Victor Felipe Jr</a>
-      <ul>
-        <li><a routerLink="/under-construction">About</a></li>
-        <li><a routerLink="/under-construction">Portfolio</a></li>
-        <li><a routerLink="/under-construction">Contact</a></li>
-      </ul>
-    </nav>
-    <div class="mobile-nav">
-      <ul>
-        <li><a routerLink="/"><img src="../assets/home-100.png" alt="Home"></a></li>
-        <li><a routerLink="/under-construction"><img class="aboutMe" src="../assets/about-me-100.png" alt="About Me"></a></li>
-        <li><a routerLink="/under-construction"><img src="../assets/portfolio-100.png" alt="Portfolio"></a></li>
-        <li><a routerLink="/under-construction"><img src="../assets/connect-100.png" alt="Connect with Me"></a></li>
-      </ul>
-    </div>
-    <div [@routeAnimations]="prepareRoute(outlet)" class="router-outlet-container">
-        <router-outlet #outlet="outlet"></router-outlet>
+    <div class="app-container">
+      <nav class="desktop-nav">
+          <a class="meHome" routerLink="/">Victor Felipe Jr</a>
+        <ul>
+          <li><a routerLink="/about">About</a></li>
+          <li><a routerLink="/portfolio">Portfolio</a></li>
+          <li><a routerLink="/contact">Contact</a></li>
+        </ul>
+      </nav>
+      <div class="mobile-nav">
+        <ul>
+          <li><a routerLink="/"><img src="../assets/home-100.png" alt="Home"></a></li>
+          <li><a routerLink="/about"><img class="aboutMe" src="../assets/about-me-100.png" alt="About Me"></a></li>
+          <li><a routerLink="/portfolio"><img src="../assets/portfolio-100.png" alt="Portfolio"></a></li>
+          <li><a routerLink="/contact"><img src="../assets/connect-100.png" alt="Connect with Me"></a></li>
+        </ul>
+      </div>
+      <div [@routeAnimations]="prepareRoute(outlet)" class="router-outlet-container">
+          <router-outlet #outlet="outlet"></router-outlet>
+      </div>
+      <app-footer></app-footer>
     </div>
 
   `,
@@ -42,10 +46,10 @@ import { trigger, transition, style, query, group, animate } from '@angular/anim
         ],{ optional: true }),
         group([
           query(':enter', [
-            animate('1s ease-in-out', style({ opacity: 1, transform: 'scale(1)' }))
+            animate('0.8s ease-in-out', style({ opacity: 1, transform: 'scale(1)' }))
           ],{ optional: true }),
           query(':leave', [
-            animate('1s ease-in-out', style({ opacity: 0, transform: 'scale(0.8)' }))
+            animate('0.8s ease-in-out', style({ opacity: 0, transform: 'scale(0.8)' }))
           ],{ optional: true })
         ])
       ])
@@ -54,7 +58,7 @@ import { trigger, transition, style, query, group, animate } from '@angular/anim
 })
 export class AppComponent {
   title: string = 'my-portfolio-website';
-  prepareRoute(outlet:any) {
-    return outlet.activatedRouteData['animation'] || 'HomePage';
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from "@angular/router";
 import { routes } from "./app/app.routes";
-
-import { provideAnimations} from "@angular/platform-browser/animations";
+import { importProvidersFrom} from "@angular/core";
+import { BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideAnimations()
+    importProvidersFrom(BrowserAnimationsModule)
   ]
 }).catch((err) => console.error(err));
